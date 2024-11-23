3a64de5e16a76542fb7e96df26ad517362101fca
tylerbartlett24 <crazyquilt86@gmail.com>
2024-11-22 21:03:10 -0330

    Added the ability to draw cells for the maze.

--- graphics.py
+++ graphics.py
@@ -1,13 +1,12 @@
 from tkinter import Tk, BOTH, Canvas
 
 class Window:
-    
     def __init__(self, width, height):
         self.__root = Tk()
-        self.__root.title = "Something"
-        self.canvas = Canvas(self.__root, height=height, width=width)
-        self.canvas.pack()
-        self.running = False
+        self.__root.title("Maze Solver")
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
         
     def redraw(self):
@@ -15,9 +14,59 @@ class Window:
         self.__root.update()
         
     def wait_for_close(self):
-        self.running = True
-        while self.running:
+        self.__running = True
+        while self.__running:
             self.redraw()
+        print("window closed...")
             
     def close(self):
-        self.running = False
+        self.__running = False
+        
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+        
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+class Line:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_color, width=2
+        )
+        
+class Cell:
+    def __init__(self, x1, x2, y1, y2, win, right=True, left=True, top=True,
+                 bottom=True):
+        self.right = right
+        self.left = left
+        self.top = top
+        self.bottom = bottom
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        self._win = win
+        
+    def draw(self):
+        if self.top:
+            self._win.draw_line(Line(Point(self._x1, self._y2),
+                                     Point(self._x2, self._y2)),
+                                "black")
+        if self.right:
+            self._win.draw_line(Line(Point(self._x2, self._y2),
+                                     Point(self._x2, self._y1)),
+                                "black")
+        if self.bottom:
+            self._win.draw_line(Line(Point(self._x1, self._y1),
+                                     Point(self._x2, self._y1)),
+                                "black")
+        if self.left:
+            self._win.draw_line(Line(Point(self._x1, self._y1),
+                                     Point(self._x1, self._y2)),
+                                "black")    
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from window import *
+from graphics import *
 
-win = Window(800, 600)
-win.wait_for_close()
+def main():
+    win = Window(800, 600)
+    cell_1 = Cell(200, 300, 400, 500, win)
+    cell_2 = Cell(450, 550, 100, 200, win, top=False, left=False)
+    cell_1.draw()
+    cell_2.draw()
+    win.wait_for_close()
+    
+main()
